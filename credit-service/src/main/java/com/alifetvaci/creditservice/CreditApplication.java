3aa7f69caa6db3988448023cf702e7930b4dcbc5
Ali Fetvacı <ali.fetvaci@getir.com>
2024-08-05 07:30:35 +0300

    credit-service start init

---
+++ credit-service/src/main/java/com/alifetvaci/creditservice/CreditApplication.java
@@ -0,0 +1,13 @@
+package com.alifetvaci.creditservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreditApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreditApplication.class, args);
+	}
+
+}
---
+++ credit-service/src/test/java/com/alifetvaci/creditservice/CreditApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alifetvaci.creditservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreditApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
