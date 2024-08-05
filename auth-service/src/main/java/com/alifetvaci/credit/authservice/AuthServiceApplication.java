9f88636844c867c8816f8e58b5e5c5c1af911122
Ali Fetvacı <ali.fetvaci@getir.com>
2024-08-05 07:25:24 +0300

    auth-service start init

---
+++ auth-service/src/main/java/com/alifetvaci/credit/authservice/AuthServiceApplication.java
@@ -0,0 +1,13 @@
+package com.alifetvaci.credit.authservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthServiceApplication.class, args);
+	}
+
+}
